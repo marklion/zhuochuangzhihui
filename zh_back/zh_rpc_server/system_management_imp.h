9fd3e91b1eeb0f7b1cf6e46c1e3990a3b1935ba6
marklion <marklion@sina.com>
2021-10-13 14:28:50 +0800

    ZCZH-15 #comment add device config from file (#10)

--- zh_back/zh_rpc_server/system_management_imp.h
+++ zh_back/zh_rpc_server/system_management_imp.h
@@ -18,6 +18,8 @@ public:
     }
     virtual bool reboot_system(const std::string &ssid);
     virtual void current_version(std::string &_return);
+    virtual void get_device_config(device_config &_return, const std::string &ssid);
+    virtual bool edit_device_config(const std::string &ssid, const device_config &config);
 };
 
 #endif // _SYSTEM_MANAGEMENT_IMP_H_
--- zh_front/zh_manage/src/views/SystemManagement.vue
+++ zh_front/zh_manage/src/views/SystemManagement.vue
@@ -1,6 +1,85 @@
 <template>
 <div class="system_management_show">
     <h2>当前版本: {{current_version}}</h2>
+
+    <el-tabs v-model="activeName">
+        <el-tab-pane label="设备配置" name="device_config">
+            <div class="device_config_show" v-for="(single_gate, index) in device_config.gate" :key="'gate' + index">
+                <el-descriptions border :title="single_gate.name">
+                    <el-descriptions-item label="入口编码">{{single_gate.entry}}</el-descriptions-item>
+                    <el-descriptions-item label="出口编码">{{single_gate.exit}}</el-descriptions-item>
+                    <el-descriptions-item label="入口身份识别IP">{{single_gate.entry_id_reader_ip}}</el-descriptions-item>
+                    <template slot="extra">
+                        <el-button type="primary" size="small" @click="open_gate_edit(single_gate)">编辑</el-button>
+                    </template>
+                </el-descriptions>
+            </div>
+            <div class="device_config_show" v-for="(single_scale, index) in device_config.scale" :key="'scale' + index">
+                <el-descriptions border :title="single_scale.name">
+                    <el-descriptions-item label="入口编码">{{single_scale.entry}}</el-descriptions-item>
+                    <el-descriptions-item label="出口编码">{{single_scale.exit}}</el-descriptions-item>
+                    <el-descriptions-item label="衡器IP">{{single_scale.scale_ip}}</el-descriptions-item>
+                    <el-descriptions-item label="光栅IP-1">{{single_scale.raster_ip[0]}}</el-descriptions-item>
+                    <el-descriptions-item label="光栅IP-2">{{single_scale.raster_ip[1]}}</el-descriptions-item>
+                    <el-descriptions-item label="入口打印机IP">{{single_scale.entry_printer_ip}}</el-descriptions-item>
+                    <el-descriptions-item label="出口打印机IP">{{single_scale.exit_printer_ip}}</el-descriptions-item>
+                    <template slot="extra">
+                        <el-button type="primary" size="small" @click="open_scale_edit(single_scale)">编辑</el-button>
+                    </template>
+                </el-descriptions>
+            </div>
+            <el-dialog @close="init_device_info" title="修改配置" :visible.sync="show_edit_gate_config" width="60%" @keyup.enter.native="edit_gate">
+                <el-form :model="gate_for_edit" ref="edit_gate_form" :rules="rules" label-width="120px">
+                    <el-form-item label="名称" prop="name">
+                        <el-input v-model="gate_for_edit.name" placeholder="请输入名称"></el-input>
+                    </el-form-item>
+                    <el-form-item label="入口编码" prop="entry">
+                        <el-input v-model="gate_for_edit.entry" placeholder="请输入入口编码"></el-input>
+                    </el-form-item>
+                    <el-form-item label="出口编码" prop="exit">
+                        <el-input v-model="gate_for_edit.exit" placeholder="请输入出口编码"></el-input>
+                    </el-form-item>
+                    <el-form-item label="入口身份识别IP" prop="entry_id_reader_ip">
+                        <el-input v-model="gate_for_edit.entry_id_reader_ip" placeholder="请输入入口身份识别IP"></el-input>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button type="primary" @click="edit_gate">确认</el-button>
+                    </el-form-item>
+                </el-form>
+            </el-dialog>
+            <el-dialog @close="init_device_info" title="修改配置" :visible.sync="show_edit_scale_config" width="60%" @keyup.enter.native="edit_scale">
+                <el-form :model="scale_for_edit" ref="edit_scale_form" :rules="rules" label-width="120px">
+                    <el-form-item label="名称" prop="name">
+                        <el-input v-model="scale_for_edit.name" placeholder="请输入名称"></el-input>
+                    </el-form-item>
+                    <el-form-item label="入口编码" prop="entry">
+                        <el-input v-model="scale_for_edit.entry" placeholder="请输入入口编码"></el-input>
+                    </el-form-item>
+                    <el-form-item label="出口编码" prop="exit">
+                        <el-input v-model="scale_for_edit.exit" placeholder="请输入出口编码"></el-input>
+                    </el-form-item>
+                    <el-form-item label="衡器IP" prop="scale_ip">
+                        <el-input v-model="scale_for_edit.scale_ip" placeholder="请输入衡器IP"></el-input>
+                    </el-form-item>
+                    <el-form-item label="光栅IP-1" prop="raster_ip">
+                        <el-input v-model="scale_for_edit.raster_ip[0]" placeholder="请输入光栅IP-1"></el-input>
+                    </el-form-item>
+                    <el-form-item label="光栅IP-2" prop="raster_ip">
+                        <el-input v-model="scale_for_edit.raster_ip[1]" placeholder="请输入光栅IP-2"></el-input>
+                    </el-form-item>
+                    <el-form-item label="入口打印机IP" prop="entry_printer_ip">
+                        <el-input v-model="scale_for_edit.entry_printer_ip" placeholder="请输入入口打印机IP"></el-input>
+                    </el-form-item>
+                    <el-form-item label="出口打印机IP" prop="exit_printer_ip">
+                        <el-input v-model="scale_for_edit.exit_printer_ip" placeholder="请输入出口打印机IP"></el-input>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button type="primary" @click="edit_scale">确认</el-button>
+                    </el-form-item>
+                </el-form>
+            </el-dialog>
+        </el-tab-pane>
+    </el-tabs>
 </div>
 </template>
 
@@ -9,18 +88,120 @@ export default {
     name: 'SystemManagement',
     data: function () {
         return {
-            current_version:'',
+            activeName:'device_config',
+            current_version: '',
+            device_config: {},
+            gate_for_edit: {},
+            scale_for_edit: {
+                raster_ip: ['', '']
+            },
+            rules: {
+                name: [{
+                    required: true,
+                    message: "请输入名称"
+                }],
+                entry: [{
+                    required: true,
+                    message: "请输入入口编码"
+                }],
+                exit: [{
+                    required: true,
+                    message: "请输入出口编码"
+                }],
+                entry_id_reader_ip: [{
+                    required: true,
+                    message: "请输入入口身份识别IP"
+                }, {
+                    pattern: /([0,1]?\d{1,2}|2([0-4][0-9]|5[0-5]))(\.([0,1]?\d{1,2}|2([0-4][0-9]|5[0-5]))){3}/g,
+                    message: '请输入正确的IP'
+                }],
+                scale_ip: [{
+                    required: true,
+                    message: "请输入衡器IP"
+                }, {
+                    pattern: /([0,1]?\d{1,2}|2([0-4][0-9]|5[0-5]))(\.([0,1]?\d{1,2}|2([0-4][0-9]|5[0-5]))){3}/g,
+                    message: '请输入正确的IP'
+                }],
+                raster_ip: [{
+                    required: true,
+                    message: "请输入光栅IP"
+                }, {
+                    pattern: /([0,1]?\d{1,2}|2([0-4][0-9]|5[0-5]))(\.([0,1]?\d{1,2}|2([0-4][0-9]|5[0-5]))){3}/g,
+                    message: '请输入正确的IP'
+                }],
+                entry_printer_ip: [{
+                    required: true,
+                    message: "请输入打印机IP"
+                }, {
+                    pattern: /([0,1]?\d{1,2}|2([0-4][0-9]|5[0-5]))(\.([0,1]?\d{1,2}|2([0-4][0-9]|5[0-5]))){3}/g,
+                    message: '请输入正确的IP'
+                }],
+                exit_printer_ip: [{
+                    required: true,
+                    message: "请输入打印机IP"
+                }, {
+                    pattern: /([0,1]?\d{1,2}|2([0-4][0-9]|5[0-5]))(\.([0,1]?\d{1,2}|2([0-4][0-9]|5[0-5]))){3}/g,
+                    message: '请输入正确的IP'
+                }],
+            },
+            show_edit_gate_config: false,
+            show_edit_scale_config: false,
         };
+    },
+    methods: {
+        edit_gate: function () {
+            var vue_this = this;
+            this.$refs.edit_gate_form.validate((valid) => {
+                if (valid) {
+                    vue_this.$call_remote_process("system_management", "edit_device_config", [vue_this.$cookies.get("zh_ssid"), vue_this.device_config]).then(function (resp) {
+                        if (resp) {
+                            vue_this.show_edit_gate_config = false;
+                        }
+                    });
+                }
+            });
+        },
+        edit_scale: function () {
+            var vue_this = this;
+            vue_this.$refs.edit_scale_form.validate((valid) => {
+                if (valid) {
+                    vue_this.$call_remote_process("system_management", "edit_device_config", [vue_this.$cookies.get("zh_ssid"), vue_this.device_config]).then(function (resp) {
+                        if (resp) {
+                            vue_this.show_edit_scale_config = false;
+                        }
+                    });
+                }
+            });
+        },
+        open_gate_edit: function (_gate) {
+            this.gate_for_edit = _gate;
+            this.show_edit_gate_config = true;
+        },
+        open_scale_edit: function (_scale) {
+            this.scale_for_edit = _scale;
+            this.show_edit_scale_config = true;
+        },
+        init_device_info: function () {
+            var vue_this = this;
+            vue_this.$call_remote_process("system_management", 'get_device_config', [vue_this.$cookies.get('zh_ssid')]).then(function (resp) {
+                vue_this.device_config = resp;
+            });
+        },
     },
     beforeMount: function () {
         var vue_this = this;
-        vue_this.$call_remote_process("system_management", 'current_version', []).then(function(resp) {
+        vue_this.$call_remote_process("system_management", 'current_version', []).then(function (resp) {
             vue_this.current_version = resp;
         });
+        this.init_device_info();
     },
 }
 </script>
 
-<style>
-
+<style scoped>
+.device_config_show {
+    border: 1px solid black;
+    margin-top: 10px;
+    padding: 5px;
+}
 </style>
