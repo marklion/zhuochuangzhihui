06a3a28f5330d751b4c2eaa992bd95a6eb2ad6ac
marklion <marklion@sina.com>
2022-03-14 23:48:24 +0800

    ZCZH-118 #comment add comunication with zyzl

--- zh_front/zh_manual_tool/src/App.vue
+++ zh_front/zh_manual_tool/src/App.vue
@@ -11,7 +11,7 @@
                             <el-input v-model="search_key" placeholder="输入部分或全部车牌搜索"></el-input>
                             <el-radio-group v-model="selected_vehicle" size="small">
                                 <div v-for="(single_vehicle, index) in vehicle_need_show" :key="index">
-                                    <el-radio :label="single_vehicle.vehicle_number" border>{{single_vehicle.vehicle_number}}</el-radio>
+                                    <el-radio :label="single_vehicle.plateNo" border>{{single_vehicle.plateNo}}</el-radio>
                                 </div>
                             </el-radio-group>
                         </el-main>
@@ -26,14 +26,14 @@
                             </div>
                             <el-button type="primary">清零</el-button>
                             <el-button v-if="!is_scaling" type="success" @click="is_scaling = true">开始称重</el-button>
-                            <el-button v-else type="danger" @click="is_scaling = false">停止称重</el-button>
+                            <el-button v-else type="danger" @click="record_weight">停止称重</el-button>
                         </el-main>
                     </el-container>
                 </el-aside>
                 <el-aside width="800px" class="print_ticket">
                     <el-container>
                         <el-header>磅单打印</el-header>
-                        <el-main >
+                        <el-main>
                             <div id="weight_ticket" class="print_content_show">
                                 <vue-drag-resize preventActiveBehavior :isActive="adjust_position" :x="350" h="auto" w="auto" :isResizable="true" parentLimitation>
                                     <div>称重单</div>
@@ -50,9 +50,9 @@
                                 </vue-drag-resize>
 
                                 <vue-drag-resize preventActiveBehavior :isActive="adjust_position" :x="300" :y="130" h="auto" w="auto" :isResizable="true" parentLimitation>
-                                    <div class="item_in_ticket">{{print_weight_content.company_name}}</div>
-                                    <div class="item_in_ticket">{{print_weight_content.vehicle_number}}</div>
-                                    <div class="item_in_ticket">{{print_weight_content.stuff_name}}</div>
+                                    <div class="item_in_ticket">{{print_weight_content.companyName}}</div>
+                                    <div class="item_in_ticket">{{print_weight_content.plateNo}}</div>
+                                    <div class="item_in_ticket">{{print_weight_content.stuffName}}</div>
                                     <div class="item_in_ticket">{{print_weight_content.j_weight}}</div>
                                     <div class="item_in_ticket">{{print_weight_content.p_weight}}</div>
                                     <div class="item_in_ticket">{{print_weight_content.p_time}}</div>
@@ -64,7 +64,7 @@
                         <el-footer>
                             <div v-if="!adjust_position">
                                 <el-button type="primary" @click="adjust_position = true">调整打印位置</el-button>
-                                <el-button type="success" v-print="'#weight_ticket'" >打印</el-button>
+                                <el-button type="success" v-print="print_obj">打印</el-button>
                             </div>
                             <div v-else>
                                 <el-button type="danger" @click="adjust_position = false">完成调整</el-button>
@@ -91,36 +91,57 @@ export default {
     name: 'App',
     data: function () {
         return {
+            print_obj: {
+                id: "weight_ticket",
+                openCallback: function (vue_this) {
+                    vue_this.init_cur_plan_data();
+                },
+            },
             adjust_position: false,
             search_key: '',
-            all_vehicle: [{
-                vehicle_number: '蒙A12345',
-                company_name: '中燃宏大',
-                stuff_name: 'LNG',
-                p_weight: 10.7,
-                p_time: '2021-12-12 21:07:21',
-                m_weight: 31.8,
-                m_time: '2021-12-12 22:10:32'
-            }, {
-                vehicle_number: '晋L88888',
-                company_name: '包头展佳',
-                stuff_name: '液氮',
-                p_weight: 15.7,
-                p_time: '2021-12-24 13:07:21',
-                m_weight: 41.8,
-                m_time: '2021-12-24 13:54:32'
-
-            }],
+            all_vehicle: [],
             selected_vehicle: '',
             is_scaling: false,
             cur_weight: 23.345,
+            stored_weight: {
+                id: "",
+                p_weight: '未知',
+                p_time: '未知',
+                m_weight: '未知',
+                m_time: '未知',
+            },
         };
+    },
+    watch: {
+        selected_vehicle: function (_selected) {
+            var vue_this = this;
+            const idb = require('idb-keyval');
+            var tmp = this.all_vehicle.find(element => {
+                return element.plateNo == _selected;
+            });
+            if (tmp) {
+                idb.get(tmp.id).then(function (resp) {
+                    if (resp) {
+                        vue_this.stored_weight = resp;
+                    }
+                });
+            } else {
+                vue_this.stored_weight = {
+                    id: "",
+                    p_weight: '未知',
+                    p_time: '未知',
+                    m_weight: '未知',
+                    m_time: '未知',
+                };
+            }
+        },
     },
     computed: {
         print_weight_content: function () {
+            var vue_this = this;
             var ret = {
-                vehicle_number: '未选择',
-                company_name: '未选择',
+                plateNo: '未选择',
+                companyName: '未选择',
                 stuff_name: '未选择',
                 p_weight: '未知',
                 p_time: '未知',
@@ -129,14 +150,22 @@ export default {
                 j_weight: '未知',
             };
             var tmp = this.all_vehicle.find(element => {
-                return element.vehicle_number == this.selected_vehicle;
+                return element.plateNo == this.selected_vehicle;
             });
+            console.log(tmp);
             if (tmp) {
                 ret = {
                     ...tmp
                 };
-                ret.j_weight = ret.m_weight - ret.p_weight;
-                ret.m_weight = this.cur_weight;
+                ret.p_weight = vue_this.stored_weight.p_weight;
+                ret.m_weight = vue_this.stored_weight.m_weight;
+                ret.p_time = vue_this.stored_weight.p_time;
+                ret.m_time = vue_this.stored_weight.m_time;
+                if (isNaN(ret.p_weight) || isNaN(ret.m_weight)) {
+                    ret.j_weight = "未知";
+                } else {
+                    ret.j_weight = ret.m_weight - ret.p_weight;
+                }
             }
 
             return ret;
@@ -145,7 +174,7 @@ export default {
             var ret = [];
             this.all_vehicle.forEach(element => {
                 if (this.search_key.length > 0) {
-                    if (PinyinMatch.match(element.vehicle_number, this.search_key)) {
+                    if (PinyinMatch.match(element.plateNo, this.search_key)) {
                         ret.push(element);
                     }
                 } else {
@@ -155,6 +184,101 @@ export default {
 
             return ret;
         },
+    },
+    methods: {
+        push_weight: function (_vehicle) {
+            var vue_this = this;
+            const axios = require('axios');
+            axios.post(vue_this.remote_path() + "/pa_web/pa_rest/push_weight?token=" + vue_this.$cookies.get("zy_token"), {
+                id: _vehicle.id,
+                pWeight: _vehicle.p_weight,
+                mWeight: _vehicle.m_weight,
+                pTime: _vehicle.pTime,
+                mTime: _vehicle.mTime,
+                jWeight: _vehicle.j_weight,
+            }).then(function (resp) {
+                console.log(resp);
+            }).catch(function (err) {
+                console.log(err);
+            });
+        },
+        record_weight: function () {
+            var vue_this = this;
+            const idb = require('idb-keyval');
+            var tmp = this.all_vehicle.find(element => {
+                return element.plateNo == this.selected_vehicle;
+            });
+            if (tmp) {
+                idb.get(tmp.id).then(function (resp) {
+                    if (resp) {
+                        var new_weight = {
+                            ...resp
+                        };
+                        new_weight.m_weight = vue_this.cur_weight;
+                        new_weight.m_time = vue_this.$make_time_string(new Date(), "-");
+                        idb.set(tmp.id, new_weight).then(function () {
+                            vue_this.stored_weight = new_weight;
+                            var push_req = {
+                                ...vue_this.stored_weight
+                            };
+                            push_req.j_weight = push_req.m_weight - push_req.p_weight;
+                            vue_this.push_weight(push_req);
+
+                        });
+                    } else {
+                        new_weight = {
+                            ...vue_this.stored_weight
+                        };
+                        new_weight.p_weight = vue_this.cur_weight;
+                        new_weight.p_time = vue_this.$make_time_string(new Date(), "-");
+                        new_weight.id = tmp.id;
+                        idb.set(tmp.id, new_weight).then(function () {
+                            vue_this.stored_weight = new_weight;
+                        });
+                    }
+                })
+            }
+            vue_this.is_scaling = false;
+        },
+        remote_path: function () {
+            return this.$remote_url;
+        },
+        init_token: function () {
+            var vue_this = this;
+            vue_this.$prompt('请输入令牌', '提示', {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                inputType: 'text',
+                inputPattern: /^[\s\S]*.*[^\s][\s\S]*$/,
+                inputErrorMessage: '请输入令牌'
+            }).then(function ({
+                value
+            }) {
+                vue_this.$cookies.set("zy_token", value, -1);
+                vue_this.init_cur_plan_data();
+            });
+        },
+        init_cur_plan_data: function () {
+            const axios = require('axios').default;
+            var vue_this = this;
+            axios.get(vue_this.remote_path() + "/pa_web/pa_rest/all_vehicle_info?token=" + vue_this.$cookies.get("zy_token")).then(function (resp) {
+                if (resp.data.err_msg != "") {
+                    vue_this.init_token();
+                } else {
+                    vue_this.all_vehicle = [];
+                    resp.data.result.forEach((element, index) => {
+                        if (element.id.slice(element.id.length - 1, element.id.length) == 'S') {
+                            vue_this.$set(vue_this.all_vehicle, index, element);
+                        }
+                    });
+                }
+            }).catch(function (err) {
+                console.log(err);
+            });
+        },
+    },
+    beforeMount: function () {
+        this.init_cur_plan_data();
     },
 }
 </script>
--- zh_front/zh_manual_tool/src/main.js
+++ zh_front/zh_manual_tool/src/main.js
@@ -1,9 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './plugins/element.js'
+import cookies from 'vue-cookies'
 
 Vue.config.productionTip = false
 
+Vue.prototype.$remote_url = process.env.VUE_APP_BACK_END_URL;
+Vue.prototype.$cookies = cookies;
+Vue.prototype.$make_time_string = function(date, _splite) {
+  var y = date.getFullYear();
+  var m = date.getMonth() + 1;
+  m = m < 10 ? ('0' + m) : m;
+  var d = date.getDate();
+  d = d < 10 ? ('0' + d) : d;
+  var h = date.getHours();
+  h = h < 10 ? ('0' + h) : h;
+  var minute = date.getMinutes();
+  minute = minute < 10 ? ('0' + minute) : minute;
+  var second = date.getSeconds();
+  second = second < 10 ? ('0' + second) : second;
+  return y + _splite + m + _splite + d + ' ' + h + ':' + minute + ':' + second;
+};
 new Vue({
   render: h => h(App),
 }).$mount('#app')
